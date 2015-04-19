c7341d194eb07d894ce30d38ddb8b98184b68050
Gustav <sir.gustav.the.coder@gmail.com>
2015-04-19 22:57:49 +0200

    glium stuff

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 extern crate sdl2;
-extern crate gl;
+extern crate glium;
+extern crate libc;
 
 use sdl2::video::{Window, WindowPos, OPENGL};
 use sdl2::keycode::KeyCode;
-
+use std::rc::Rc;
+use glium::Surface;
 
 pub fn main() {
   let sdl_context = sdl2::init(sdl2::INIT_VIDEO).unwrap();
@@ -14,38 +16,60 @@ pub fn main() {
   sdl2::video::gl_set_attribute(sdl2::video::GLAttr::GLDoubleBuffer, 1);
   sdl2::video::gl_set_attribute(sdl2::video::GLAttr::GLDepthSize, 24);
   
-  let window = match Window::new(&sdl_context, "game", WindowPos::PosCentered, WindowPos::PosCentered, 800, 600, OPENGL) {
+  let mut window = match Window::new(&sdl_context, "game", WindowPos::PosCentered, WindowPos::PosCentered, 800, 600, OPENGL) {
     Ok(window) => window,
     Err(err) => panic!("Failed to create window: {}", err)
   };
-  
   let context = window.gl_create_context().unwrap();
-  gl::load_with(|s| unsafe {
-    std::mem::transmute(sdl2::video::gl_get_proc_address(s))
-  });
-  
-  let mut running = true;
   let mut event_pump = sdl_context.event_pump();
-
-  while running {
+  let dsize = window.properties(&event_pump).get_drawable_size();
+  let rcwindow = Rc::new(window);
+  
+  struct Backend {
+    window: Rc<Window>,
+    size : (i32, i32),
+  }
+  unsafe impl glium::backend::Backend for Backend {
+    fn swap_buffers(&self) {
+        self.window.gl_swap_window();
+    }
+    unsafe fn get_proc_address(&self, symbol: &str) -> *const libc::c_void {
+      std::mem::transmute(sdl2::video::gl_get_proc_address(symbol))
+    }
+    fn get_framebuffer_dimensions(&self) -> (u32, u32) {
+      let size = self.size;
+      let (w,h) = size;
+      (w as u32, h as u32)
+    }
+    fn is_current(&self) -> bool {
+      false
+    }
+    unsafe fn make_current(&self) {
+      let gl_context = match sdl2::video::gl_get_current_context() {
+        Ok(context) => context,
+        Err(err) => panic!("Failed to get GL context: {}", err)
+      };
+      self.window.gl_make_current(&gl_context);
+    }
+  }
+  let context = unsafe {
+      glium::backend::Context::new(Backend { window:rcwindow.clone(), size:dsize }, false)
+    }.unwrap();
+  
+  loop {
     for event in event_pump.poll_iter() {
       use sdl2::event::Event;
+      
+      let mut target = glium::Frame::new(context.clone(), context.get_framebuffer_dimensions());
+      target.clear_color(0.0, 1.0, 0.0, 1.0);
+      target.finish();
 
       match event {
         Event::Quit {..} | Event::KeyDown { keycode: KeyCode::Escape, .. } => {
-          running = false
+          return;
         },
         _ => {}
       }
     }
-    
-    // The rest of the game loop goes here...
-    unsafe {
-      // Clear the screen to black
-      gl::Viewport(0, 0, 800, 600);
-      gl::ClearColor(0.0, 0.0, 0.9, 1.0);
-      gl::Clear(gl::COLOR_BUFFER_BIT);
-    }
-    window.gl_swap_window();
   }
 }
