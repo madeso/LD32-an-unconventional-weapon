3a31b8af0b5139a1e409b6c468029845bb2b3185
Gustav <sir.gustav.the.coder@gmail.com>
2015-04-20 19:23:46 +0200

    added basic file loading

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,31 @@ use sdl2::keycode::KeyCode;
 use std::rc::Rc;
 use glium::Surface;
 
+fn load_file_to_string(file_path : &str) -> String {
+  use std::error::Error;
+  use std::fs::File;
+  use std::io::prelude::*;
+  use std::path::Path;
+  
+  let path = Path::new(file_path);
+  let display = path.display();
+
+  // Open the path in read-only mode, returns `io::Result<File>`
+  let mut file = match File::open(&path) {
+      // The `description` method of `io::Error` returns a string that
+      // describes the error
+      Err(why) => panic!("couldn't open {}: {}", display, Error::description(&why)),
+      Ok(file) => file,
+  };
+
+  // Read the file contents into a string, returns `io::Result<usize>`
+  let mut s = String::new();
+  match file.read_to_string(&mut s) {
+      Err(why) => panic!("couldn't read {}: {}", display, Error::description(&why)),
+      Ok(_) => s,
+  }
+}
+
 pub fn main() {
   let sdl_context = sdl2::init(sdl2::INIT_VIDEO).unwrap();
   
